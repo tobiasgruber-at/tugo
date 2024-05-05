ca19b24445d532899ee89393212bde70baf30750
Tobias Gruber <tgruber.at@gmail.com>
2024-05-05 16:26:37 +0200

    fix: db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_29_160953) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_142443) do
   create_table "favorites", force: :cascade do |t|
     t.string "item_id"
     t.integer "user_id", null: false
@@ -23,11 +23,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_29_160953) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password", null: false
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "favorites", "users"
