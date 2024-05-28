e88f06f220497e46963f18aaa57e4ef7bed8599c
Tobias Gruber <tgruber.at@gmail.com>
2024-05-28 14:14:36 +0200

    feat: map favorites for favorite report

--- app/controllers/favorites_report_controller.rb
+++ app/controllers/favorites_report_controller.rb
@@ -1,6 +1,56 @@
 class FavoritesReportController < TissApiController
    layout "report"
 
-   def index()
-   end
+  def index()
+    begin
+      @favorites = Favorite.where(user_id: session[:user_id]).order(preview: :asc)
+      @favorite_courses = map_resources(
+        @favorites&.filter { |fav| fav.favorite_type == "course" },
+        -> (id) { course_path(id) }
+      )
+      @favorite_people = map_resources(
+        @favorites&.filter { |fav| fav.favorite_type == "person" },
+        -> (id) { person_path(id) }
+      )
+      @favorite_theses = map_resources(
+        @favorites&.filter { |fav| fav.favorite_type == "thesis" },
+        -> (id) { thesis_path(id) }
+      )
+      @favorite_projects = map_resources(
+        @favorites&.filter { |fav| fav.favorite_type == "project" },
+        -> (id) { project_path(id) }
+      )
+    rescue StandardError => e
+      puts e.message
+      @favorites = nil
+      flash.now[:alert] = "An error occurred. Please try again later."
+    end
+  end
+
+  def map_resources(favorites, path_selector_fn)
+    if favorites.nil? || favorites.empty?
+      []
+    else
+      favorites.map do |fav|
+        id = fav["item_id"]
+        path = path_selector_fn.call(id)
+        keywords = fav.nil? ? nil : Keyword.where(favorite_id: id)
+        map_resource(fav, path, keywords)
+      end
+    end
+  end
+
+  def map_resource(fav, path, keywords)
+    id = fav["id"] || ""
+    Resource.new(
+      id: id,
+      title: fav["preview"],
+      _prefix: nil,
+      addition: nil,
+      path: path,
+      favorite: fav,
+      favorite_type: nil,
+      keywords: keywords
+    )
+  end
 end
---
+++ test/controllers/favorites_report_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FavoritesReportControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
